98ca1159b025d354ddb467e2f90b99495c5917db
Skyler Lan <bluezod@gmail.com>
2018-02-05 15:34:50 +1300

    skip the index record if image file could no be found #2

--- multi_compress.py
+++ multi_compress.py
@@ -32,7 +32,7 @@ def compress(args: list):
         print("{0} of {1} processed; Progress: {2}%".format(i.value, total, percentage))
 
     path = args[1]
-    if os.path.isfile(path):
+    if not os.path.isfile(path):
         return '|'.join([args[0], path, 'Original file not found'])
     new_path = __get_new_path(path)
     command_called = False
